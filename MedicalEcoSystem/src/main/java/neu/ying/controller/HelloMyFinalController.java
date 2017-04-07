f2417c46786ecb3e4ffd380811af25c57336b746
Ying Wang <150336414@qq.com>
2017-04-07 11:09:35 -0400

    Initial of my webtool final project. LOL

---
+++ MedicalEcoSystem/src/main/java/neu/ying/controller/HelloMyFinalController.java
@@ -0,0 +1,16 @@
+package neu.ying.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloMyFinalController {
+	@RequestMapping(value = "index", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("message", "lol hi...get my final started");
+		return "hello";
+	}
+}
---
+++ MedicalEcoSystem/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,6 @@
+
+<html>
+<body>
+	<h1>message: ${message }</h1>
+</body>
+</html>
