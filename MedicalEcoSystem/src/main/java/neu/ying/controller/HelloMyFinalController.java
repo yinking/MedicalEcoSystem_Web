533529bb7566bd1dae7bd051b1efd66a747cf9a1
Ying Wang <150336414@qq.com>
2017-04-10 13:14:53 -0400

    login page

--- MedicalEcoSystem/src/main/java/neu/ying/controller/HelloMyFinalController.java
+++ MedicalEcoSystem/src/main/java/neu/ying/controller/HelloMyFinalController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/hello")
 public class HelloMyFinalController {
-	@RequestMapping(value = "index", method = RequestMethod.GET)
+	@RequestMapping(value = "index", method = RequestMethod.POST)
 	public String index(Model model) {
 		model.addAttribute("message", "lol hi...get my final started");
 		return "hello";
